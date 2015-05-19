8cecba76c1d1d845dc71ebdecd3f04e3bd748d5e
Dirk Moors <dirkmoors@gmail.com>
2015-05-19 21:24:13 +0200

    Added some documentation

--- src/pyejabberd/client.py
+++ src/pyejabberd/client.py
@@ -10,8 +10,30 @@ from .core.errors import IllegalArgumentError
 
 # noinspection PyTypeChecker
 class EjabberdAPIClient(contract.EjabberdAPIContract):
+    """
+    Python Client for the Ejabberd XML-RPC API
+    """
     def __init__(self, host, port, username, password, xmpp_domain, muc_service="conference", protocol='https',
                  verbose=False):
+        """
+        Constructor
+        :param host:
+        :type host: str
+        :param port:
+        :type port: int
+        :param username:
+        :type username: str
+        :param password:
+        :type password: str
+        :param xmpp_domain:
+        :type xmpp_domain: str
+        :param muc_service:
+        :type muc_service: str
+        :param protocol:
+        :type protocol: str
+        :param verbose:
+        :type verbose: bool
+        """
         self.host = host
         self.port = port
         self.username = username
@@ -23,14 +45,28 @@ class EjabberdAPIClient(contract.EjabberdAPIContract):
 
     @property
     def service_url(self):
+        """
+        Returns the FQDN to the Ejabberd server's XML-RPC endpoint
+        :return:
+        """
         return "%s://%s:%s/" % (self.protocol, self.host, self.port)
 
     @property
     def proxy(self):
+        """
+        Returns the proxy object that is used to perform the calls to the XML-RPC endpoint
+        :rtype: :py:class:xmlrpclib.ServerProxy
+        :return the proxy object that is used to perform the calls to the XML-RPC endpoint
+        """
         return xmlrpclib.ServerProxy(self.service_url, verbose=(1 if self.verbose else 0))
 
     @property
     def auth(self):
+        """
+        Returns a dictionary containing the basic authorization info
+        :rtype: dict
+        :return: a dictionary containing the basic authorization info
+        """
         return {
             'user': self.username,
             'server': self.xmpp_domain,
@@ -38,6 +74,14 @@ class EjabberdAPIClient(contract.EjabberdAPIContract):
         }
 
     def _call_api(self, api_class, **kwargs):
+        """
+        Internal method used to perform api calls
+        :param api_class:
+        :type api_class: py:class:API
+        :param kwargs:
+        :type kwargs: dict
+        :return:
+        """
         assert issubclass(api_class, API)
 
         # Create api instance
