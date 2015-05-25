c2371c69f5bb6666600278e2a84772e832dbfa52
Dirk Moors <dirkmoors@gmail.com>
2015-05-25 22:08:47 +0200

    Implementing fix for urllib3 ssl warning

--- src/pyejabberd/__init__.py
+++ src/pyejabberd/__init__.py
@@ -1,3 +1,10 @@
 __version__ = "0.2.1"
 
+try:  # pragma: no cover
+    # https://urllib3.readthedocs.org/en/latest/security.html#pyopenssl
+    import urllib3.contrib.pyopenssl
+    urllib3.contrib.pyopenssl.inject_into_urllib3()
+except ImportError:
+    pass
+
 from .client import EjabberdAPIClient
